219b301641164b60f830ccdddf95ef73ba7eefab
Terry9022 <karta1020376@gmail.com>
2024-04-08 20:48:24 -0400

    update the frontend and output

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import xgboost as xgb
-from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn import metrics
 import numpy as np
 
 app = Flask(__name__)
@@ -27,32 +25,12 @@ def diagnose():
     yprob = np.argmax(y_pred, axis=1)
     predictions = [round(value) for value in yprob]
 
-    # Calculate performance metrics
-    test_fault_y = pd.read_csv('fault7_output.csv')
-    acc, recall, f1, precision, confusion_mat = show_performance(test_fault_y, predictions)
-
     # Prepare the response
     response = {
-        'predictions': predictions,
-        'accuracy': acc,
-        'recall': recall,
-        'f1_score': f1,
-        'precision': precision,
-        'confusion_matrix': confusion_mat.tolist()
+        'predictions': predictions
     }
 
     return jsonify(response)
-
-def show_performance(label, preds):
-    accuracy = accuracy_score(label, preds)
-
-    acc = accuracy * 100.0
-    recall = metrics.recall_score(label, preds, average='macro')
-    f1 = metrics.f1_score(label, preds, average='macro')
-    precision = metrics.precision_score(label, preds, average='macro')
-    res_confusion_matrix = confusion_matrix(label, preds)
-
-    return acc, recall, f1, precision, res_confusion_matrix
 
 if __name__ == '__main__':
     app.run(debug=True)
